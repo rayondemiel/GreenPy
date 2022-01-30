59144b205098786acca57c6a14ab28a16d4fc49b
rayondemiel <maximehumeau44@gmail.com>
2022-01-30 18:42:09 +0100

    connexion sqlite

--- run.py
+++ run.py
@@ -2,32 +2,49 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask("GreenPY")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/env.db'
 db = SQLAlchemy(app)
 
 class Acteur(db.Model):
     id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
     nom = db.Column(db.Text, nullable=False)
     prenom = db.Column(db.Text, nullable=False)
-    date_naissance = db.Column(db.string(4), nullable=False)
-    date_deces = db.Column(db.string(4))
+    date_naissance = db.Column(db.Text, nullable=False)
+    date_deces = db.Column(db.Text)
     ville_naissance = db.Column(db.Text, nullable=False)
     pays_naissance = db.Column(db.Integer, nullable=False)
-    profession = db.Column(db.string(45))
+    profession = db.Column(db.Text)
     biographie = db.Column(db.Text, nullable=False)
+
+"""class Objet_contest(db.model):
+    id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
+    nom = db.Column(db.Text, nullable=False)
+    categ_id = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    date_debut = db.Column(db.Text, nullable=False)
+    date_fin = db.Column(db.Text)
+    ville = db.Column(db.Text, nullable=False)
+    dpt = db.Column(db.Text)
+    pays_id = db.Column(db.Integer, nullable=False)
+    ressources = db.Column(db.Text)
+    img_id = db.Column(db.Integer)"""
 
 @app.route("/")
 @app.route("/accueil")
 def accueil():
-    return render_template("pages/accueil.html", name="accueil", militants=militants, projets_contest=projets_contest)
+    militants = Acteur.query.all()
+    """projets_contest = Objet_contest.query.all()"""
+    return render_template("pages/accueil.html", name="accueil", militants=militants)#, projets_contest=projets_contest)
 
 @app.route("/militant/<int:name_id>")
 def militant(name_id):
-    return render_template("pages/militant.html", name="militant", militant=militants[name_id])
+    militants = Acteur.query.get(name_id)
+    return render_template("pages/militant.html", name="militant", militant=militants)
 
-@app.route("/projet_contest/<int:objContest_id>")
+"""@app.route("/projet_contest/<int:objContest_id>")
 def objContest(objContest_id):
-    return render_template("pages/objet_contest.html", name="objet_contest", projet_contest=projets_contest[objContest_id])
+    projets_contest = Objet_contest.query.get(objContest_id)
+    return render_template("pages/objet_contest.html", name="objet_contest", projet_contest=projets_contest)"""
 
 if __name__ == "__main__":
     app.run(debug=True)
