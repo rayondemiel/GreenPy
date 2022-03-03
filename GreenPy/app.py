e4173e23270efd036d6e81baeed45d8ba9084b67
rayondemiel <maximehumeau44@gmail.com>
2022-03-03 18:08:37 +0100

    ajout users

--- GreenPy/app.py
+++ GreenPy/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 import os
+
+from .constantes import SECRET_KEY
 
 chemin_actuel = os.path.dirname(os.path.abspath(__file__))
 templates = os.path.join(chemin_actuel, "templates")
@@ -9,9 +12,13 @@ statics = os.path.join(chemin_actuel, "static")
 app = Flask("GreenPy",
     template_folder=templates,
     static_folder=statics)
+#Instanciation SECRET KEY
+app.config['SECRET_KEY'] = SECRET_KEY
 # On configure la base de données
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/env.db'
 # On initie l'extension
 db = SQLAlchemy(app)
+
+login = LoginManager(app)
 
 from .routes import routes
--- GreenPy/constantes.py
+++ GreenPy/constantes.py
@@ -1,1 +1,11 @@
+from warnings import warn
+
+#Constantes des résultats de recherche
 RESULTATS_PAR_PAGES = 10
+
+
+#Secret Key
+SECRET_KEY = "Pensez Printemps, les amis !!!"
+
+if SECRET_KEY == "Pensez Printemps, les amis !!!":
+    warn("Le secret par défaut n'a pas été changé, vous devriez le faire", Warning)
